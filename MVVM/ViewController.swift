84c6f831b7645bbc904fa1b0e04aa2cadc7612b7
Sugeng Wibowo <swibowo@bbmtek.com>
2020-02-06 22:04:13 +0700

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Sugeng Wibowo on 06/02/20.
+//  Copyright © 2020 Sugeng Wibowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
